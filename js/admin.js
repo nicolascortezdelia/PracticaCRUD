8f3b3269881c8c0ce5b88d3a8624bfbd5feb3a59
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2021-11-09 22:16:39 -0300

    agrego comentarios de funciones

--- js/admin.js
+++ js/admin.js
@@ -15,6 +15,12 @@ let campoCodigo = document.querySelector("#codigoID");
 //console.log(campoCodigo)
 
 //MANEJADORES DE EVENTO
+//para AGREGAR el evento al campo
+//con esto digo: al <input> -campocodigo- le quiero agregar un evento
+//me pide DOS PARÁMETROS
+//parámetro 1: ¿qué evento quiero agregar? R: el BLUR -sin el ON porque no lo traigo desde el HTML-
+//parámetro 2: ¿qué quiero asociar cuando se produzca el evento BLUR...
+//...en el input? r: EL NOMBRE DE LA FUNCIÓN o sea campoRequerido
 
 campoCodigo.addEventListener("blur", () =>{campoRequerido (campoCodigo)});
 //cuando se haga el blur que se ejecute la función campoRequerido
